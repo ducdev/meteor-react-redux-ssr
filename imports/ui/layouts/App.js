aaf36c4fed5afade2df3825e5575c9103f494259
Duc Le <leanhducvt@live.com>
2019-12-28 23:25:06 +0700

    Update 2019 (#3)

--- imports/ui/layouts/App.js
+++ imports/ui/layouts/App.js
@@ -1,20 +1,17 @@
-import React, { PropTypes } from 'react'
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
-import { Provider } from 'react-redux'
+import React from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 
-import Index from '../pages/Index'
 import Posts from '../pages/Posts'
 import Post from '../pages/Post'
-import configureStore from '../../store/configure-store'
-
-const store = configureStore()
 
 const App = () => (
   <div className="App">
-    <h1><Link to='/'>Meteor + React + Redux + SSR</Link></h1>
+    <h1><Link to='/'>Meteor + React + Redux + SSR 2020</Link></h1>
     <Switch>
-      <Route exact name="index" path="/" component={() => <Redirect to="/posts"/>} />
+      <Route exact path="/">
+        <Redirect to="/posts"/>
+      </Route>
       <Route exact name="posts" path="/posts" component={Posts} />
       <Route exact name="post" path="/post/:slug" component={Post} />
     </Switch>
--- imports/ui/pages/Posts.js
+++ imports/ui/pages/Posts.js
@@ -1,37 +1,32 @@
-import React, { Component } from 'react'
-import { bindActionCreators } from 'redux'
+import React from 'react'
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 import { Link } from 'react-router-dom'
 
 import { fetchPosts } from '../../actions/posts'
 
-class Posts extends Component {
-  componentWillMount() {
-    const { fetchPosts, postsList } = this.props
-    if (!postsList || postsList.length === 0)
-      fetchPosts()
+const Posts = ({ loading, fetchPosts, postsList }) => {
+  if (loading || !postsList || postsList.length === 0) {
+    fetchPosts()
+    console.log('fetchPosts()')
+    return <div>loading...</div>
   }
-  render() {
-    const { loading, postsList } = this.props
-    if (loading || !postsList || postsList.length === 0)
-      return <div>loading...</div>
-    return (
-      <div>
-        {
-          postsList &&
-          postsList.length > 0 &&
-          <ul>
-            {
-              postsList.map(post => <li key={post.title}>
-                <h3><Link to={`/post/${post.slug}`}>{post.title}</Link> - <i>by {post.author} ({post.votes} vote{ post.votes !== 1 ? 's' : ''})</i></h3>
-                <h5>{post.description}</h5>
-              </li>)
-            }
-          </ul>
-        }
-      </div>
-    )
-  }
+  return (
+    <div>
+      {
+        postsList &&
+        postsList.length > 0 &&
+        <ul>
+          {
+            postsList.map(post => <li key={post.title}>
+              <h3><Link to={`/post/${post.slug}`}>{post.title}</Link> - <i>by {post.author} ({post.votes} vote{ post.votes !== 1 ? 's' : ''})</i></h3>
+              <h5>{post.description}</h5>
+            </li>)
+          }
+        </ul>
+      }
+    </div>
+  )
 }
 
 function mapStateToProps(state) {
@@ -42,10 +37,8 @@ function mapStateToProps(state) {
   }
 }
 
-function mapDispatchToProps(dispatch) {
-  return {
-    fetchPosts: bindActionCreators(fetchPosts, dispatch)
-  }
-}
+const mapDispatchToProps = dispatch => ({
+  fetchPosts: bindActionCreators(fetchPosts, dispatch)
+})
 
 export default connect(mapStateToProps, mapDispatchToProps)(Posts)
